34882d5a09f61c0bf9ed8ad6277caa2289d11f30
Abdolbaghi Teygh khorshid <34252946+Teygh@users.noreply.github.com>
2018-06-25 11:55:02 -0400

    Final Portfolio page being  uploaded

---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+$(window).on("load", function () {
+    $(".loader").fadeOut(2000);
+})
+
+
+$(document).ready(function () {
+
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer.", "Programmer.", "Petroleum Engineer.", "Chemical Engineer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        navigation: true,
+        slideSpeed:2000,
+        autoplay: true,
+        autoplaySpeed:10000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+
+        }
+
+    });
+
+    $("#filters a").click(function () {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+    
+            }
+        });
+
+        return false;
+    });
+
+    $("#navigation li a").click(function (e) {
+    e.preventDefault();
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition -50}, "slow");
+    })
+
+    const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+
+
+
+
+	}
+
+    
+});
